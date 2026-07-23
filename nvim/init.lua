d6cac43a3c6fd55a5224fdadb1a53cd1e35942ec
ChaoticQubit <chaoticqubit@gmail.com>
2026-07-23 00:37:48 -0400

    Adding Nvim plugins and navigation plugin setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,9 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.vim'
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   spec = "plugins",
-  lockfile = vim.fn.stdpath("state") .. "./lazy-lock.json",
+  lockfile = vim.fn.stdpath("state") .. "/lazy-lock.json",
 })
