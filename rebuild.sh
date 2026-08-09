ec0f506e28d050016f5c45c0d66be084f5a98af0
ChaoticQubit <chaoticqubit@gmail.com>
2026-08-09 10:35:23 -0400

    Updating Homebrew before Rebuilding

--- rebuild.sh
+++ rebuild.sh
@@ -2,4 +2,5 @@
 set -euo pipefail
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 ln -sfn "$DIR" ~/.dotfiles
+( cd "$DIR" && nix flake update nix-homebrew )
 exec sudo nix run nix-darwin/master#darwin-rebuild -- switch --flake ~/.dotfiles#myMac
